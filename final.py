7c493066ec260f25bc0871bc9cdb273b51b56d3d
sanket rukade <mysoftwaysanket@gmail.com>
2026-03-03 18:08:58 +0530

    Improved logic in final.py

--- final.py
+++ final.py
@@ -1753,6 +1753,7 @@ LEVEL_PATTERNS = [
 SKIP_LINES = {"D", "i", "Stage", "rState Common Entrance Test Cell"}
 
 
+
 # =====================================================
 # CORE FUNCTION (REUSED)
 # =====================================================
@@ -1883,6 +1884,7 @@ def extract_pdf_to_csv_bytes(pdf_bytes):
                                     perc,
                                 ])
                         break
+                    
 
     # -------- WRITE CSV IN MEMORY --------
     csv_buffer = io.StringIO()
